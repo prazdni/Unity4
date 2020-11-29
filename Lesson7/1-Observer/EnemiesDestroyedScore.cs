47e622d079542dd61b0363f1f43973054037259c
Danil <danilstasilevich@yandex.ru>
2020-11-29 16:35:37 +0700

    Added lesson 7 Scripts

---
+++ Lesson7/1-Observer/EnemiesDestroyedScore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Asteroids
+{
+    public class EnemiesDestroyedScore : ShowEnemyInfo
+    {
+        private TMP_Text _score;
+        
+        private void Start()
+        {
+            _score = GetComponent<TMP_Text>();
+            _score.text = "0";
+        }
+
+        public override void ShowInfo(EnemyEventInfo info)
+        {
+            if (!info.IsOutOfBounds)
+            {
+                _score.text = (Convert.ToInt32(_score.text) + 1).ToString();
+            }
+        }
+    }
+}
---
+++ Lesson7/1-Observer/EnemyEventInfo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Asteroids
+{
+    public struct EnemyEventInfo
+    {
+        public Transform SceneObjectTransform { get; }
+        public bool IsOutOfBounds { get; }
+
+        public EnemyEventInfo(Transform obj, bool isOutOfBounds)
+        {
+            SceneObjectTransform = obj;
+            IsOutOfBounds = isOutOfBounds;
+        }
+    }
+}
